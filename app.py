ea27a7eecf4b72c47edd09afc3e0a7ecaa58c642
Matt Ball <yosemitebandit@gmail.com>
2012-09-22 03:48:04 -0700

    adds more structure

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+''' meta routes
+'''
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+''' player data routes
+'''
+@app.route('/players/<name>/questions')
+def player_questions(name):
+    return render_template('player_questions.html', name=name)
+
+@app.route('/players/<name>/dashboard')
+def player_dashboard(name):
+    return render_template('player_dashboard.html', name=name)
+
+@app.route('/players/<name>')
+def player_home(name):
+    return render_template('player_home.html', name=name)
+
+
+''' API routes
+'''
+@app.route('/api/players/<player_id>')
+def player_data(player_id):
+    pass
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
---
+++ readme.md
@@ -0,0 +1,16 @@
+
+## interface routes
+```
+/
+/about
+/faq
+/players/<player-name>
+/players/<player-name>/questions
+/players/<player-name>/dashboard
+```
+
+## API routes
+```
+/api/players/<player-id>
+```
+
